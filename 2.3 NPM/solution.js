bdb087056ac7f4f04a9bd338f873d3af61852f14
njflash88 <gordonchu88@yahoo.com>
2025-07-28 15:52:17 +0800

    course done upload

--- 2.3 NPM/solution.js
+++ 2.3 NPM/solution.js
@@ -1,12 +1,9 @@
 // var generateName = require("sillyname");
-import generateName from "sillyName";
-
+import generateName from "sillyname";
+import superheros from "superheros";
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
 
-// const superheroes = require("superheroes");
-import superheroes from "superheroes";
-
-const name = superheroes.random();
-console.log(`I am ${name}!`);
+// const name = superheros.random();
+// console.log(`I am ${name}!`);
--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -5,3 +5,7 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
+
+app.get("/", (req, res) => {
+  console.log(req.rawHeaders);
+});
